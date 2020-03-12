08018a5f30a7750ab401c53f6bf73de03ba83508
Daylan <gobstys@gmail.com>
2020-03-12 16:19:57 -0600

    Connection Fix

--- RemoteApp/RemoteApp/RemoteApp/Views/ConnectionPage.xaml.cs
+++ RemoteApp/RemoteApp/RemoteApp/Views/ConnectionPage.xaml.cs
@@ -22,31 +22,54 @@ namespace RemoteApp.Views
             string s = "";
             IPAddress ip;
             bool ValidateIP = IPAddress.TryParse(IP.Text, out ip);
+            bool go = true;
             if (!ValidateIP)
                 alertIP();
             else
             {
                 try
                 {
-                    Int32 port = 8883;
-                    TcpClient client = new TcpClient("10.0.0.250", port);
+                    IPAddress IPP;
+                    if (IPAddress.TryParse(IP.Text, out IPP))
+                    {
+                        Socket ss = new Socket(AddressFamily.InterNetwork,
+                        SocketType.Stream,
+                        ProtocolType.Tcp);
+                        ss.ReceiveTimeout = 3000;
+                        ss.SendTimeout = 3000;
+                        try
+                        {
+                            ss.Connect(IPP, 8883);
+                            ss.Close();
+                            go = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            go = false;
+                        }
+                    }
+                    if (go)
+                    {
+                        Int32 port = 8883;
+                        TcpClient client = new TcpClient(IP.Text, port);
 
-                    Byte[] data = System.Text.Encoding.ASCII.GetBytes("login,Hoff");
+                        Byte[] data = System.Text.Encoding.ASCII.GetBytes("login,Hoff");
 
-                    NetworkStream stream = client.GetStream();
-                    stream.Write(data, 0, data.Length);
+                        NetworkStream stream = client.GetStream();
+                        stream.Write(data, 0, data.Length);
 
-                    Console.WriteLine("Sent: {0}", "Login,Hoff");
-                    data = new Byte[256];
+                        Console.WriteLine("Sent: {0}", "Login,Hoff");
+                        data = new Byte[256];
 
-                    String responseData = String.Empty;
+                        String responseData = String.Empty;
 
-                    Int32 bytes = stream.Read(data, 0, data.Length);
-                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                    s = responseData;
+                        Int32 bytes = stream.Read(data, 0, data.Length);
+                        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                        s = responseData;
 
-                    stream.Close();
-                    client.Close();
+                        stream.Close();
+                        client.Close();
+                    }
                 }
                 catch (ArgumentNullException ex)
                 {
